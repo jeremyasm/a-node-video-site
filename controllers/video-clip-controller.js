0662a306d0faa862dfa16f0b4fc8c8771840e0c8
jeremyasm <liyaoxing1991@126.com>
2017-11-15 23:48:04 +0800

    add getClipsByFrames function and refine comments and logs

--- controllers/video-clip-controller.js
+++ controllers/video-clip-controller.js
@@ -1,5 +1,4 @@
 const path = require('path');
-// const cutVideo = require('../util/video-cut');
 var fs = require("fs");
 var ffmpeg = require('fluent-ffmpeg');
 
@@ -7,19 +6,15 @@ module.exports.helloWorld = function(req, res) {
   res.send('Hello World!')
 }
 
+// GET /:inputFileName/byTime?startTimestamp=***&endTimestamp=***
 module.exports.clipByTime = function(req, res, err) {
 
-  console.log("Get video clip by starttime and endtime ...");
+  console.log("GET /:inputFileName/byTime?startTimestamp=***&endTimestamp=***");
 
   var inputFileName = req.params.inputFileName;
   var startTimestamp = req.query.startTimestamp ? req.query.startTimestamp : '00:00:03';
   var endTimestamp = ''; //TODO
   var duration = req.query.duration ? req.query.duration : 10;
-
-  // var startTimestamp = '00:00:03';
-  // var endTimestamp = '';
-  // var duration = 11;
-  // var inputFileName = 'sample.mp4';
 
   //TODO convert duration to endtime ?
   //moment ???
@@ -28,14 +23,12 @@ module.exports.clipByTime = function(req, res, err) {
   // if file exists ?
   // if startTimestamp > video.length ?
 
-  //TODO format the name of output file
-
   console.log("inputFileName: " + inputFileName);
   console.log("startTimestamp: " + startTimestamp);
   console.log("endTimestamp: " + endTimestamp);
   console.log("duration: " + duration);
 
-  //TODO to be refined
+  //TODO format the name of output file
   var str = startTimestamp.split(':')[0] + startTimestamp.split(':')[1] + startTimestamp.split(':')[2];
   // var str = ":33:332:231";
   var outputFileName  = inputFileName.split('.')[0] + '-clip-' + str + '+' + duration + '.mp4';
@@ -70,29 +63,25 @@ module.exports.clipByTime = function(req, res, err) {
 
 }
 
-// TODO
+// GET /:inputFileName/byFrames?startFrame=***&endFrame=***
 module.exports.clipByFrames = function(req, res, err) {
 
-  console.log("Get video clip by start frame and end frame ...");
-
-  var startFrame = req.query.startFrame;
-  var endFrame = req.query.endFrame;
+  console.log("GET /:inputFileName/byFrames?startFrame=***&endFrame=***");
 
   var inputFileName = req.params.inputFileName;
+  var startFrame = req.query.startFrame;
+  var endFrame = req.query.endFrame;
 
   console.log("startFrame: " + startFrame);
   console.log("endFrame: " + endFrame);
 
   //TODO validations
   // if file exists ?
-  // if startTimestamp > video.length ?
 
   var outputFileName  = inputFileName.split('.')[0] + '-frames-from' + startFrame + 'to' + endFrame + '.mp4';
   // var outputFileName  = "output-frame-test.mp4";
 
-  // TODO how to get the originial frame count ???
-  // var frameCount = ffmpeg(path.resolve(process.env.VIDEO_INPUT_PATH, inputFileName));
-
+  // STEP 1. get frame rate from metadata of origin video using ffprobe
   ffmpeg.ffprobe(path.resolve(process.env.VIDEO_INPUT_PATH, inputFileName), function(err, metadata) {
     if (err) console.log(err);
     var videoStreamObj = metadata.streams[0].codec_type === "video" ? metadata.streams[0] : metadata.streams[1];
@@ -106,12 +95,16 @@ module.exports.clipByFrames = function(req, res, err) {
     console.log("avg_frame_rate: " + originAvgFrameRateStr + " => " + originAvgFrameRate);
     console.log("nb_frames: " + originFrames);
 
+    // TODO validations
+    // if (startFrame, endFrame) is out of the range of nb_frames ?
+
     var startTimestamp = startFrame / originAvgFrameRate;
     var duration = (endFrame - startFrame)  / originAvgFrameRate;
 
     console.log("startTimestamp: " + startTimestamp);
     console.log("duration: " + duration);
 
+    // STEP 2. get video clip by startTimestamp and duration using ffmpeg
     ffmpeg(path.resolve(process.env.VIDEO_INPUT_PATH, inputFileName))
       .setFfmpegPath(process.env.FFMPEG_PATH)
       .setStartTime(startTimestamp)
@@ -140,9 +133,13 @@ module.exports.clipByFrames = function(req, res, err) {
 
 }
 
+// GET /:inputFileName/frames
 module.exports.getFrames = function(req, res, err) {
 
+  console.log("GET /:inputFileName/byFrames?startFrame=***&endFrame=***");
+
   var inputFileName = req.params.inputFileName;
+
   ffmpeg.ffprobe(path.resolve(process.env.VIDEO_INPUT_PATH, inputFileName), function(err, metadata) {
     if (err) console.log(err);
     var videoStreamObj = metadata.streams[0].codec_type === "video" ? metadata.streams[0] : metadata.streams[1];
@@ -161,20 +158,112 @@ module.exports.getFrames = function(req, res, err) {
   });
 }
 
-// GET /select
+// GET /:fileName/select
 module.exports.initSelectPage = function(req, res, err) {
+
+  console.log("GET /:fileName/select");
+
   var fileName = req.params.fileName;
   res.sendFile(path.resolve(__dirname + '/../public/select_highlights_type.html'));
 }
 
-// GET /:fileName/highlights?highlightsType=xxx
+// GET /:fileName/highlights?highlightsType=***
 module.exports.getHighlightsFromVideo =  function(req, res, err) {
+
+  console.log("GET /:fileName/highlights?highlightsType=***");
+
   var fileName = req.params.fileName;
   var highlightsType = req.query.highlightsType;
-  // TODO
-  // process video and get specific type of highlights
+
+  // TODO process video and get specific type of highlights, return a list of (startFrame, endFrame) tuples
+  var framesArr = mockupImageIdentificationService(fileName, highlightsType);
+  var tmpStart, tmpEnd, tmpStrArr;
+  for(var i = 0; i < framesArr.length; i ++){
+    tmpStrArr = framesArr[i].split('-');
+    tmpStart = + tmpStrArr[0];
+    tmpEnd = + tmpStrArr[1];
+    getClipByFrames(fileName, tmpStart, tmpEnd);
+  }
+
+  // TODO return complete message when all the clips complete
   res.json({
     "fileName" : fileName,
     "highlightsType" : highlightsType
   });
+}
+
+
+function mockupImageIdentificationService(fileName, highlightsType){
+
+  console.log("mockupImageIdentificationService is processing video: ");
+  console.log("filename: " + fileName);
+  console.log("highlightsType: " + highlightsType);
+
+  var framesArray = ["5-25", "50-59", "100-200"];
+  return framesArray;
+}
+
+function getClipByFrames(inputFileName, startFrame, endFrame) {
+
+  console.log("getClipByFrames:");
+
+  console.log("inputFileName: " + inputFileName);
+  console.log("startFrame: " + startFrame);
+  console.log("endFrame: " + endFrame);
+
+  var mark = inputFileName + "-" + "from" + startFrame + "to" + endFrame;
+
+  //TODO validations
+  // if file exists ?
+
+  var outputFileName  = "HIGHLIGHTS-" + inputFileName.split('.')[0] + '-frames-from' + startFrame + 'to' + endFrame + '.mp4';
+  // var outputFileName  = "output-frame-test.mp4";
+
+  // STEP 1. get frame rate from metadata of origin video using ffprobe
+  ffmpeg.ffprobe(path.resolve(process.env.FILE_UPLOAD_PATH, inputFileName), function(err, metadata) {
+    if (err) console.log(err);
+    var videoStreamObj = metadata.streams[0].codec_type === "video" ? metadata.streams[0] : metadata.streams[1];
+    var originRFrameRateStr = videoStreamObj.r_frame_rate;
+    var originRFrameRate = originRFrameRateStr.split('/')[0] / originRFrameRateStr.split('/')[1];
+    var originAvgFrameRateStr = videoStreamObj.avg_frame_rate;
+    var originAvgFrameRate =  originAvgFrameRateStr.split('/')[0] / originAvgFrameRateStr.split('/')[1];
+    var originFrames = videoStreamObj.nb_frames;
+    // console.log(metadata);
+    console.log("r_frame_rate: " + originRFrameRateStr + " => " + originRFrameRate);
+    console.log("avg_frame_rate: " + originAvgFrameRateStr + " => " + originAvgFrameRate);
+    console.log("nb_frames: " + originFrames);
+
+    var startTimestamp = startFrame / originAvgFrameRate;
+    var duration = (endFrame - startFrame)  / originAvgFrameRate;
+
+    console.log("startTimestamp: " + startTimestamp);
+    console.log("duration: " + duration);
+
+    // STEP 2. get video clip by startTimestamp and duration using ffmpeg
+    ffmpeg(path.resolve(process.env.FILE_UPLOAD_PATH, inputFileName))
+      .setFfmpegPath(process.env.FFMPEG_PATH)
+      .setStartTime(startTimestamp)
+      .setDuration(duration)
+      .on('progress', function(info) {
+        console.log(mark + ' ' + 'progress ' + info.percent.toFixed(2) + '%' + ', ' + 'processed frames: ' + info.frames);
+      })
+      .on('error', function(err) {
+        console.error(mark + ' ' + 'An error occurred: ' + err.message);
+        // res.status(500);
+        // res.json({
+        //   message: err.message,
+        // }); // res.json end
+      })
+      .on('end', function(stdout, stderr) {
+        console.log(mark + ' ' + 'Processing finished !');
+        // res.status(200);
+        // res.json({
+        //   message: 'Processing finished !',
+        //   outputLocation: path.resolve(process.env.VIDEO_OUTPUT_PATH, outputFileName)
+        // }); // res.json end
+      })
+      .save(path.resolve(process.env.VIDEO_OUTPUT_PATH, outputFileName));
+
+  });
+
 }
